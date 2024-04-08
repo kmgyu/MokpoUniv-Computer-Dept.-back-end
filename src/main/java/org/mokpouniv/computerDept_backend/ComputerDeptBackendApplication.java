6df13da85dc6642b5024d821ecaa2ed7347826a9
kmgyu <lidersy961@gmail.com>
2024-04-08 21:11:03 +0900

    [Test] First Commit Test

---
+++ src/main/java/org/mokpouniv/computerDept_backend/ComputerDeptBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mokpouniv.computerDept_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerDeptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerDeptBackendApplication.class, args);
+	}
+
+}
