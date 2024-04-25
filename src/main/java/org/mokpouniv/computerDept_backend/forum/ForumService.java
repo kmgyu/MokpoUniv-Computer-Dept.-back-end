84644bc24bba2e0ee174bcabf235f872741aa45e
kmgyu <lidersy961@gmail.com>
2024-04-25 22:08:44 +0900

    몽고DB atlas 연동 테스트

--- src/main/java/org/mokpouniv/computerDept_backend/forum/ForumService.java
+++ src/main/java/org/mokpouniv/computerDept_backend/forum/ForumService.java
@@ -1,4 +1,30 @@
 package org.mokpouniv.computerDept_backend.forum;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class ForumService {
+
+    ForumRepository forumRepo;
+
+    public boolean addItem(ForumEntity forumEntity) {
+        try {
+            forumRepo.save(forumEntity);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<String> getItemNames() {
+        return forumRepo.findAll().stream().map(ForumEntity::getTitle).collect(Collectors.toList());
+    }
+
 }
