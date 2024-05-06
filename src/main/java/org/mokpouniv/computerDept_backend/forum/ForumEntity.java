72885b2dea6e5b56ac159f8688955a1a34516a38
Myung-Il <ktr040415@gmail.com>
2024-05-06 23:14:45 +0900

    [feat] 게시판 CRUD
    - url의 행위는 아직 지우지 않음
    - NoticeController, NoticeDTO 추가 하지 않음
    
    create
    아이디, 제목, 작성자, 내용을 적어야 됨
    하나라도 적지 않았을 경우 작성되지 않음
    
    read
    제목을 기준으로 관련 게시글을 찾아주고 없을 경우 아무것도 뜨지 않음
    
    delete
    id를 기준으로 해당 게시글을 지우고 true를 반환
    id가 없다면 false를 반환
    
    update
    id를 기준으로 게시글이 있다면 제목과 내용만 수정가능하다
    만약 없는 id라면 수정되지 않는다
    
    get-names
    저장되어 있는 모든 제목만을 보여준다

--- src/main/java/org/mokpouniv/computerDept_backend/forum/ForumEntity.java
+++ src/main/java/org/mokpouniv/computerDept_backend/forum/ForumEntity.java
@@ -4,12 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.UUID;
-
 @Document("Notice")
 @NoArgsConstructor
-@AllArgsConstructor
-//@RequiredArgsConstructor
 @Data
 @Builder
 public class ForumEntity {
@@ -19,14 +15,10 @@ public class ForumEntity {
     private String author; // 게시물 작성자
     private String content; // 게시물 내용
 
-//    public ForumEntity(String title, String author, String content) {
-//        this.id = UUID.randomUUID().toString(); // UUID를 사용하여 고유한 아이디 생성
-//        this.title = title;
-//        this.author = author;
-//        this.content = content;
-//    }
-
-    public ForumDTO toForumDTO() {
-        return new ForumDTO(title, author, content);
+    public ForumEntity(String id, String title, String author, String content) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.content = content;
     }
 }
