2efc0bfe1eba68612203d8267f62505b27e7a722
kimSummerrain <doeun457@gmail.com>
2024-05-11 17:37:10 +0900

    [feat] 게시판 R - forum service에 findAllByTitleRegex라는 메서드로 바꿈 - forum repository에서 $regex를 활용하여 Title에 입력된 값이 DB에 저장되어있는 Title 속성 값에 포함되어 있으면 출력하게 바꿈

--- src/main/java/org/mokpouniv/computerDept_backend/forum/ForumRepository.java
+++ src/main/java/org/mokpouniv/computerDept_backend/forum/ForumRepository.java
@@ -8,13 +8,16 @@ import java.util.List;
 
 @Repository
 public interface ForumRepository extends MongoRepository<ForumEntity, String> {
-    @Query("{title:'?0'}")
-    ForumEntity findForumEntityByName(String title);
 
-    @Query(value = "{author:'?0'}",fields="{'content':1}")
-    List<ForumEntity>findAllBy(String author);
 
-    ForumEntity findForumEntityByTitle(String title);
+    public interface ForumEntityRepository extends MongoRepository<ForumEntity, String> {
+
+        // 정규 표현식을 사용하여 단어를 포함하는 모든 title을 조회하는 쿼리
+        @Query(value = "{ 'title': { $regex: ?0, $options: 'i' } }")
+        List<ForumEntity> findAllByTitleRegex(String keyword);
+    }
+
+    ForumEntity findAllByTitleRegex(String title);
 
     public long count();
 }
