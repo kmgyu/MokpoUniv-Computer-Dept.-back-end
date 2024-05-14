f3fbac4d6cfae16a6b8b92e2d07db0afa9afdd61
kmgyu <lidersy961@gmail.com>
2024-05-14 22:54:13 +0900

    [feat] comment 패키지 추가 - notice 콜렉션 네임 수정 - 검색 쿼리 수정

--- src/main/java/org/mokpouniv/computerDept_backend/forum/notice/NoticeEntity.java
+++ src/main/java/org/mokpouniv/computerDept_backend/forum/notice/NoticeEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document("Notice")
+@Document("notice")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
