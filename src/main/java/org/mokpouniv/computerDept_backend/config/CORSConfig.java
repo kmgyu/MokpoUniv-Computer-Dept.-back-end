867bcbce072465ecba92636ecb3aec1b14c24569
kmgyu <lidersy961@gmail.com>
2024-06-02 16:45:30 +0900

    [chore] cors config origin 3

--- src/main/java/org/mokpouniv/computerDept_backend/config/CORSConfig.java
+++ src/main/java/org/mokpouniv/computerDept_backend/config/CORSConfig.java
@@ -22,7 +22,7 @@ public class CORSConfig {
 //        source.registerCorsConfiguration("/**",config);
 
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("localhost:8080");
         config.setAllowedMethods(List.of("POST", "OPTIONS", "GET", "DELETE", "PUT"));
         config.setAllowedHeaders(List.of("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         source.registerCorsConfiguration("/**", config);
